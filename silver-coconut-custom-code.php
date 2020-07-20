d0559d87482879fb97aef7dfb99538ae613e2417
Lem <lemsmyth@yahoo.com>
2020-07-19 22:21:55 -0500

    create main php file and plugin comment block

---
+++ silver-coconut-custom-code.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: ❤︎❤︎ SilverCoconut.com Custom Code
+ * Plugin URI: https://lemsmyth.com/plugins/site-specific-custom-code/
+ * Description: Add custom site specific code here.
+ * Version: 0.1.0
+ * Author: lemsmyth
+ * Author URI: https://lemsmyth.com/
+ * Donate Link: https://lemsmyth.com/donate/
+ * Requires PHP: 5.6.20
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: lems
+ * Domain Path: /languages
+ */
+if ( !defined( 'ABSPATH' ) ) exit;
